299cb305b933eaabc7880ec303917e4f932b8784
Ralf Hochhausen <micro@ralf-hochhausen.de>
2019-12-09 11:36:21 +0100

    implemented simple blinky, updated debug config

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,32 @@
   * @brief   Default main function.
   ******************************************************************************
 */
+#include "stm32f4xx.h"
+#include <cstdint>
+
+static void HWSetup(void)  {
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+	GPIOD->ODR   |= 0x0000F000;
+	GPIOD->MODER |= 0x55000000;
+}
 
 int main(void)
 {
-	for(;;) {
+	uint32_t i;
+	HWSetup();
 
+	for(;;) {
+		for(i = 1000000; i>0; i--) {
+
+		}
+
+		GPIOD->BSRR = 0xF0000000;
+
+		for(i = 1000000; i>0; i--) {
+
+		}
+
+		GPIOD->BSRR = 0x0000F000;
 	}
 
 	return 0;
