cee6f738f436b7558ba79292390fc4d52586075d
Ralf Hochhausen <micro@ralf-hochhausen.de>
2020-12-20 13:42:07 +0100

    renamed compiler specifc toolchain file

--- cmake/cm4.cmake
+++ cmake/cm4.cmake
@@ -10,4 +10,4 @@ set(MCPU_FLAGS "-mthumb -mcpu=cortex-m4")				# Basic core related flags
 set(VFP_FLAGS "-mfpu=fpv4-sp-d16 -mfloat-abi=hard")		# Floating point configuration
 set(SPEC_FLAGS "")										# e.g. "--specs=nosys.specs" or "--specs=newlib.nano"
 
-include(${CMAKE_CURRENT_LIST_DIR}/toolchain.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/gcc-arm-none-eabi.cmake)
