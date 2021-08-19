d848568802e219b2a46f807325ada60f7f109fb8
Ralf Hochhausen <micro@ralf-hochhausen.de>
2021-08-19 18:58:02 +0200

    added first tests

--- components/observer/test/ObserverTest.cpp
+++ components/observer/test/ObserverTest.cpp
@@ -1,1 +1,30 @@
+#include "ISubject.h"
+#include <gmock/gmock.h>
 #include "gtest/gtest.h"
+
+using testing::Test;
+using ::testing::Eq;
+
+namespace Observer {
+    namespace testing {
+	    class ObserverTest : public Test {
+	    protected:
+		    ObserverTest(){}
+		    ~ObserverTest(){}
+            
+            Subject_t subject;
+
+		    virtual void SetUp(){}
+		    virtual void TearDown(){}
+	    };
+
+        TEST_F(ObserverTest, constructor_foo) {
+		    EXPECT_THAT(true, Eq(true));
+	    }
+
+        TEST_F(ObserverTest, constructor_bar) {
+		    EXPECT_THAT(true, Eq(false));
+	    }
+
+    }   // namespace testing
+}   // namespace Observer
