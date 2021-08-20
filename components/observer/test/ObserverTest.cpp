9aa2428bc31541f6b633365ab7720a382357ca97
Ralf Hochhausen <micro@ralf-hochhausen.de>
2021-08-20 11:04:18 +0200

    updated test list

--- components/observer/test/ObserverTest.cpp
+++ components/observer/test/ObserverTest.cpp
@@ -15,8 +15,13 @@ namespace Observer {
 		    ~ObserverTest(){}
             
             Subject_t subject;
+            Observer_t observer_1;
+            Observer_t observer_2;
+            Observer_t observer_3;
 
 		    virtual void SetUp(){
+                /*  Init subject with dummy values in order to simulate that
+                    the subject might not be initialized by default */
                 subject.pHead   = (Observer_t*)1234;
                 subject.cnt     = 1;
             }
@@ -33,6 +38,19 @@ namespace Observer {
             OBS_CreateSubject(&subject);
 		    EXPECT_THAT(subject.pHead, IsNull());
             EXPECT_THAT(subject.cnt, Eq(0));
+	    }
+
+        TEST_F(ObserverTest, ObserverIsNotInitializedBeforeCreation) {
+		    EXPECT_THAT(observer_1.p_next,  NotNull());
+            EXPECT_THAT(observer_1.p_prev,  NotNull());
+            EXPECT_THAT(observer_1.p_cb,    NotNull());
+	    }
+
+        TEST_F(ObserverTest, ObserverIsInitializedAfterCreation) {
+            OBS_CreateSubject(&subject);
+		    EXPECT_THAT(observer_1.p_next,   IsNull());
+            EXPECT_THAT(observer_1.p_prev,  IsNull());
+            EXPECT_THAT(observer_1.p_cb,    IsNull());
 	    }
 
     }   // namespace testing
