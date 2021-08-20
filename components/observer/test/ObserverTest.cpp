64df098a6451afc4c9132e8575f6b9bcf0c1420d
Ralf Hochhausen <micro@ralf-hochhausen.de>
2021-08-20 11:28:25 +0200

    made all tests pass

--- components/observer/test/ObserverTest.cpp
+++ components/observer/test/ObserverTest.cpp
@@ -3,11 +3,12 @@
 #include "gtest/gtest.h"
 
 using testing::Test;
-using ::testing::Eq;
+using testing::Eq;
 using testing::IsNull;
 using testing::NotNull;
+using testing::TypedEq;
 
-namespace Observer {
+namespace ObserverUnitTest {
     namespace testing {
 	    class ObserverTest : public Test {
 	    protected:
@@ -24,6 +25,10 @@ namespace Observer {
                     the subject might not be initialized by default */
                 subject.pHead   = (Observer_t*)1234;
                 subject.cnt     = 1;
+
+                observer_1.p_next   = (Observer_t*)1234;
+                observer_1.p_prev   = (Observer_t*)1234;
+                observer_1.p_cb     = (Observer_cb_t)0;
             }
 
 		    virtual void TearDown(){}
@@ -43,14 +48,14 @@ namespace Observer {
         TEST_F(ObserverTest, ObserverIsNotInitializedBeforeCreation) {
 		    EXPECT_THAT(observer_1.p_next,  NotNull());
             EXPECT_THAT(observer_1.p_prev,  NotNull());
-            EXPECT_THAT(observer_1.p_cb,    NotNull());
+            EXPECT_THAT(observer_1.p_cb,    IsNull());
 	    }
 
         TEST_F(ObserverTest, ObserverIsInitializedAfterCreation) {
-            OBS_CreateSubject(&subject);
-		    EXPECT_THAT(observer_1.p_next,   IsNull());
+            OBS_CreateObserver(&observer_1, (Observer_cb_t)4321);
+		    EXPECT_THAT(observer_1.p_next,  IsNull());
             EXPECT_THAT(observer_1.p_prev,  IsNull());
-            EXPECT_THAT(observer_1.p_cb,    IsNull());
+            EXPECT_THAT(observer_1.p_cb,    NotNull());
 	    }
 
     }   // namespace testing
